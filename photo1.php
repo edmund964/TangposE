1b78966053f17f0b6d382ebc226f6334ad88a484
edmund964 <54048224+edmund964@users.noreply.github.com>
2019-08-13 10:31:11 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -6,6 +6,6 @@
 <body>  
 
 <h1> Tangpos Gallery </h1> 
-  <img src=""> 
+  <img src="https://github.com/edmund964/TangposE/blob/gallery/38469359_419584418563942_3797970087281950720_o.jpg"> 
   </body> 
   </html>

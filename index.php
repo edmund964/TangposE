0a61940d286a6cb1720892e30a59c872de593b7c
edmund964 <54048224+edmund964@users.noreply.github.com>
2019-08-13 09:33:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<a href="">photo#1</a> 
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Jose Edmund A. Tangpos</title> 
+</head> 
+<body> 
+
+<h1>Tangpos Photo Gallery</h1> 
+<a href="">photo#1</a>
+<a href="">photo#2</a> 
+<a href="">photo#3</a>
+</body>
+</html>
